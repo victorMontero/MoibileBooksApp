22b4c81c2d631151a0d702158508c021e561ddf5
Montero <victorhugo.montero@thomsonreuters.com>
2022-07-25 14:15:41 -0300

    retofit instance

--- app/src/main/java/com/example/moibilebooksapp/api/BooksApi.kt
+++ app/src/main/java/com/example/moibilebooksapp/api/BooksApi.kt
@@ -1,4 +1,17 @@
 package com.example.moibilebooksapp.api
 
+import com.example.moibilebooksapp.models.BookList
+import com.example.moibilebooksapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface BooksApi {
+
+
+    @GET(" ")
+    suspend fun getBooks(
+        @Query("q") inTitle: String = "mobile",
+        @Query("key") apiKey: String = API_KEY
+    ): Response<BookList>
 }
--- app/src/main/java/com/example/moibilebooksapp/util/Constants.kt
+++ app/src/main/java/com/example/moibilebooksapp/util/Constants.kt
@@ -1,4 +1,9 @@
 package com.example.moibilebooksapp.util
 
 class Constants {
+
+     companion object {
+          const val API_KEY = "AIzaSyBkK9KjG373ytb5yXpSCLp2rE_XH1FyMT4"
+          const val BASE_URL = "https://www.googleapis.com/books/v1/volumes/"
+     }
 }
